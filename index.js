d8657f453168f8e989d3dded8ad826ebbd63c3cf
Bhavya Dua <dua_bhavya@outlook.com>
2022-10-06 16:00:47 -0400

    commit tree 6e2adf28f6052acfe94bd2b1ed1a4a4d836c8b65

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(drivers, filter) {
+    return drivers.filter(driver => driver.toLowerCase() === filter.toLowerCase());
+}
+
+function fuzzyMatch(drivers, filter) {
+    return drivers.filter(driver => driver.substring(0, filter.length) === filter);
+}
+
+function matchName(drivers, filter) {
+    return drivers.filter(driver => driver.name === filter);
+}
